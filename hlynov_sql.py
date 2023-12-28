33ba8d72b3cbbe8e3c495f56033f595415360058
Кожин Роман <resume-gl@yandex.ru>
2023-12-28 16:44:58 +0300

    вынес скрипты работы с скл в отдельный файл, сделал работу через контекстный менеджер

--- hlynov_sql.py
+++ hlynov_sql.py
@@ -1,11 +1,13 @@
 import sqlite3
 import datetime
 import logging
-
+import os
+from sql_script_SBP import SBP_create, SBP_add_doc, SBP_all_doc, SBP_find_doc, SBP_add_column_sum
 
 current_time = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H_%M_%S')
+
 logging.basicConfig(
-    filename='D:\\files\\' + __file__ + "_" + current_time + '_.log',
+    filename='D:\\files\\' + os.path.basename(__file__) + "_" + current_time + '_.log',
     filemode='a',
     level=logging.DEBUG,
     format="%(asctime)s - %(filename)s - %(funcName)s: %(lineno)d - %(message)s",
@@ -14,45 +16,80 @@ logging.basicConfig(
 
 class DocumentsDB:
     def __init__(self, db_path: str = 'd:\\kassa\\db_receipt\\hlynov_bd.db'):
-        self.conn = sqlite3.connect(db_path)
+        self.db_path = db_path
         self.create_table()
+        self.add_column_sum()
+
+    def get_connection(self):
+        """
+        метод получения соединения с базой данных
+        :return:
+        """
+        return sqlite3.connect(self.db_path)
 
     def create_table(self):
-        cursor = self.conn.cursor()
-        cursor.execute("""
-            CREATE TABLE IF NOT EXISTS documents (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                date DATE,
-                sbis_id TEXT,
-                qrc_id TEXT
-            )
-        """)
-        logging.debug('создали БД')
-        self.conn.commit()
+        """
+        метод создания таблицы
+        :return: None
+        """
+        with self.get_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute(SBP_create)
+            logging.debug('создали БД')
+            conn.commit()
 
-    def add_document(self, date: str = '2022-01-01', sbis_id: str ='1/01', qrc_id: str = '99999'):
-        cursor = self.conn.cursor()
-        cursor.execute("""
-            INSERT INTO documents (date, sbis_id, qrc_id) VALUES (?, ?, ?)
-        """, (date, sbis_id, qrc_id))
-        self.conn.commit()
-        logging.debug('добавили запись в таблицу {0}={1}'.format(sbis_id, qrc_id))
+    def add_column_sum(self):
+        """
+        метод добавления столбца суммы в таблицу
+        :return:
+        """
+        try:
+            with self.get_connection() as conn:
+                cursor = conn.cursor()
+                cursor.execute(SBP_add_column_sum)
+                logging.debug('добавили столбец суммы')
+                conn.commit()
+        except Exception as exc:
+            logging.debug(f'ошибка {exc}')
+
+    def add_document(self,
+                     date: str = '2022-01-01',
+                     sbis_id: str ='1/01',
+                     qrc_id: str = '99999',
+                     sum: int = 0):
+        """
+        метод добавления документа в таблицу
+        """
+        with self.get_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute(SBP_add_doc, (date, sbis_id, qrc_id, sum))
+            conn.commit()
+            logging.debug('добавили запись в таблицу {0}={1}'.format(sbis_id, qrc_id))
 
     def get_documents(self):
-        cursor = self.conn.cursor()
-        cursor.execute("SELECT * FROM documents")
-        rows = cursor.fetchall()
+        """
+        метод получения всех записей из таблицы
+        :return:
+        """
+        with self.get_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute(SBP_all_doc)
+            rows = cursor.fetchall()
         return rows
 
     def find_document(self, date: str = '2022-01-01', sbis_id: str = '1/01'):
-        cursor = self.conn.cursor()
-        cursor.execute("""
-            SELECT qrc_id FROM documents
-            WHERE date = ? AND sbis_id = ?
-        """, (date, sbis_id))
-        row = cursor.fetchone()
-        logging.debug('нашли запись в таблиц {0}={1}'.format(sbis_id, row))
-        return row[0] if row else None
+        """
+        метод поиска qrcID в системе СБП документа по его дате и номеру в сбис
+        :param date: str дата продажи в формате YYYY-MM-DD
+        :param sbis_id: str номер чека в сбис
+        :return: str
+        """
+        with self.get_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute(SBP_find_doc, (date, sbis_id))
+            row = cursor.fetchone()
+            logging.debug('нашли запись в таблиц {0}={1}'.format(sbis_id, row))
+            return row[0] if row else None
 
 
 def main():
