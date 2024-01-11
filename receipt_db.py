3d992fc6a50151341e0307edf6c413198a59516a
Кожин Роман <resume-gl@yandex.ru>
2024-01-11 11:47:48 +0300

    вынес скрипты sql в отдельный файл

--- receipt_db.py
+++ receipt_db.py
@@ -3,145 +3,22 @@ import logging
 import json
 import os
 from typing import Dict, List
-SQL_DROP_TABLE = """
-    DROP TABLE IF EXISTS receipt;
-    DROP TABLE IF EXISTS items;
-    DROP TABLE IF EXISTS bonusi;
-"""
-sql_make_db = """
-            CREATE TABLE IF NOT EXISTS receipt (
-                id VARCHAR(20) PRIMARY KEY,
-                number_receipt VARCHAR(20),
-                date_create VARCHAR(8),
-                shop_id INTEGER,
-                sum REAL,
-                SumBeforeSale INTEGER,
-                clientID VARCHAR(12),
-                inn_pman VARCHAR(12),
-                phone VARCHAR(11),
-                bonus_add INTEGER,
-                bonus_dec INTEGER,
-                bonus_begin VARCHAR(8),
-                bonus_end VARCHAR(8),
-                operation_type VARCHAR(12)
-            );
-            CREATE TABLE IF NOT EXISTS items (
-                id VARCHAR(20),
-                nn VARCHAR(20),
-                barcode VARCHAR(31),
-                artname VARCHAR(255), 
-                name VARCHAR(255),
-                quantity INTEGER,
-                price REAL,
-                seller VARCHAR(20),
-                comment VARCHAR(20),
-                FOREIGN KEY (id) REFERENCES 'receipt' (id)
-                    ON DELETE CASCADE
-            );
-            CREATE TABLE IF NOT EXISTS bonusi (
-                id_receipt VARCHAR(20),
-                id_akcii INTEGER,
-                bonus_begin VARCHAR(8),
-                bonus_end VARCHAR(8),
-                bonus_add INTEGER,
-                FOREIGN KEY (id_receipt) REFERENCES 'receipt' (id)
-                    ON DELETE CASCADE
-            );
+from sql_script_for_receipt_db import \
+    (SQL_DROP_TABLE, sql_make_db,
+     sql_update_db_bonus_begin,
+     sql_update_db_operation_type,
+     sql_update_db_bonus_end,
+     sql_update_db_items,
+     sql_add_document,
+     sql_add_item,
+     sql_add_bonusi,
+     sql_delete_document,
+     sql_get_document,
+     sql_get_items,
+     sql_get_bonusi,
+     SQL_COUNT
+     )
 
-        """
-sql_update_db_bonus_begin = """
-            ALTER TABLE receipt
-            ADD COLUMN bonus_begin VARCHAR(8)
-        """
-sql_update_db_operation_type = """
-            ALTER TABLE receipt
-            ADD COLUMN operation_type VARCHAR(12)
-        """
-
-sql_update_db_bonus_end = """
-            ALTER TABLE receipt
-            ADD COLUMN bonus_end VARCHAR(8)
-        """
-sql_update_db_items = """
-            ALTER TABLE items
-            ADD COLUMN artname VARCHAR(254);
-        """
-sql_add_document = """
-            INSERT INTO receipt (
-                id,
-                number_receipt,
-                date_create,
-                shop_id,
-                sum,
-                SumBeforeSale,
-                clientID,
-                inn_pman,
-                phone,
-                bonus_add,
-                bonus_dec,
-                bonus_begin,
-                bonus_end,
-                operation_type) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
-
-"""
-sql_add_item = """
-            INSERT INTO items (
-                id,
-                nn,
-                barcode,
-                artname,
-                name,
-                quantity,
-                price,
-                seller,
-                comment) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);
-
-"""
-sql_add_bonusi = f"""
-            INSERT INTO bonusi (
-                id_receipt, 
-                id_akcii, 
-                bonus_begin, 
-                bonus_end, 
-                bonus_add) VALUES (?, ?, ?, ?, ?); 
-"""
-
-sql_delete_document = """
-            DELETE FROM receipt WHERE id = ?;
-"""
-sql_get_document = """
-            SELECT id, 
-            number_receipt, 
-            date_create, 
-            shop_id, 
-            sum, 
-            SumBeforeSale, 
-            clientID, 
-            inn_pman, 
-            phone, 
-            bonus_add, 
-            bonus_dec,
-            bonus_begin,
-            bonus_end,
-            operation_type
-            FROM receipt
-            LIMIT 10;
-"""
-sql_get_items = """
-            SELECT id, nn, barcode, artname, name, quantity, price, seller, comment
-            FROM items
-            WHERE id = ?;
-"""
-
-sql_get_bonusi = """
-            SELECT id_receipt, id_akcii, bonus_begin, bonus_end, bonus_add
-            FROM bonusi
-            WHERE id_receipt = ?;
-"""
-
-SQL_COUNT = """
-            SELECT COUNT(*) FROM receipt 
-"""
 
 id_operation = {
     'sale': 's',
