f2ac40b4b126ecf12834f191104e5533614206a2
Кожин Роман <resume-gl@yandex.ru>
2024-10-21 11:12:03 +0300

    исправил подготовку КМ чтобы делала замену на GS символы только в одном, первом шаблоне, а не во всех найденных

--- preparation_km_to_honest_sign.py
+++ preparation_km_to_honest_sign.py
@@ -10,17 +10,19 @@ def preparation_km(in_km: str) -> str:
     """
     pattern = r'91\S+?92'
     s_break = '\x1D'
-    list_break_pattern = re.findall(pattern, in_km[30:])
+    strt_symbol = 30
+    fnsh_symbol = 40
+    list_break_pattern = re.findall(pattern, in_km[strt_symbol:fnsh_symbol])
     if len(list_break_pattern) > 0:
         repl = (s_break + list_break_pattern[0]).replace('92', s_break + '92')
-        out_km = in_km[:30] + re.sub(pattern, repl, in_km[30:])
+        out_km = in_km[:strt_symbol] + re.sub(pattern, repl, in_km[strt_symbol:fnsh_symbol]) + in_km[fnsh_symbol:]
     else:
         out_km = in_km[:]
     return out_km
 
 
 def main():
-    km = 'oolololo'
+    km = '0104610293561292215Oa3NcYIG,-c>91EE1092p91P92bWgYS4Xcjcr+OWglRNO7lp/W0aFgCUQM9Ay14='
     preparation_km(in_km=km)
 
 
