2b0ffc8d026dac03360697b62112da97d8c140bf
Кожин Роман <resume-gl@yandex.ru>
2026-03-11 12:50:15 +0300

    pinpad добавил возможность сделать сверку итогов отдельно2

--- pinpad_OOP.py
+++ pinpad_OOP.py
@@ -10,7 +10,7 @@ DICT_OPERATION_CHECK = {'sale': 4000,
                         'correct_sale': 128,
                         'correct_return_sale': 130,
                         'x_otchet': 7004,
-                        'reconciliation': 7004,
+                        'reconciliation': 6000, #сверка итогов отдельно
                         'full_otchet': 6002,
                         'z_otchet': 6000}
 
